82adbd210414db7eae0e5243c97f0f7f6c167bbd
Ilycas <itchyshit310@gmail.com>
2026-06-07 15:58:30 -0400

    redes y dsitribucion de paquetes
    h

--- modem_principal.gd
+++ modem_principal.gd
@@ -1,12 +1,13 @@
 extends Node3D
 
 @export var antena_principal : Node3D
+@export var porcentaje_asignacion : float = 0.5 
 
-var ancho_banda_central : float = 300.0
+var ancho_banda_central : float = 0.0
 
 func _process(_delta: float) -> void:
 	if is_instance_valid(antena_principal):
-		ancho_banda_central = antena_principal.ancho_banda_entrada
+		ancho_banda_central = antena_principal.ancho_banda_entrada * porcentaje_asignacion
 	else:
 		ancho_banda_central = 0.0
 
