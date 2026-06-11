8134d358598ad759ea4402aa1fea2e9056c27672
Ilycas <itchyshit310@gmail.com>
2026-06-10 20:33:56 -0400

    decoracion
    decoracion

--- antena_principal.gd
+++ antena_principal.gd
@@ -1,8 +1,8 @@
 extends MeshInstance3D
 
-@export var ancho_banda : float = 300.0
+@export var ancho_banda : float = 500.0
 
-var ancho_banda_entrada : float = 300.0
+var ancho_banda_entrada : float = 500.0
 
 func _ready() -> void:
 	ancho_banda_entrada = ancho_banda
